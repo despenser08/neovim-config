f852504a9ea535efdd8a00f73b6f9e6d8e8e3003
despenser08 <despenser08@gmail.com>
2024-04-15 00:11:46 -0500

    feat(toggleterm): add terminal keymap

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,1 +1,9 @@
-return { "akinsho/toggleterm.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons", opts = {} }
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    require("toggleterm").setup {}
+    vim.keymap.set("n", "<leader>T", "<cmd>ToggleTerm<CR>", { desc = "[T]oggle Terminal" })
+  end,
+}
